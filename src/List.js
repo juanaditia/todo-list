278a7c9753c82a43f1ac4fe99e630b837e0f0b0c
Juan <juanaditia94@gmail.com>
2022-01-01 22:10:37 +0700

    remove wrong code

---
+++ src/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function List(props) {
+  const handleRemoveItem = () => {
+    console.log('hapus');
+    alert(
+      'delet ora bisa angel wis tak goleti nang stack overvllow ora slove ngederi dong bsia bantu chat wa wkowowko'
+    );
+  };
+
+  return (
+    <ul>
+      {props.items.map((item, index) => (
+        <li key={index}>
+          {' '}
+          {item}
+          <button onClick={handleRemoveItem}>delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default List;
