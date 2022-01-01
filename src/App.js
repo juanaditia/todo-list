c7876ce58e4c514dd8bbc14ef2de11950ee7700a
Juan <juanaditia94@gmail.com>
2022-01-01 22:13:37 +0700

    fiks bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import List from './List';
 
 class App extends Component {
